9cacd5a0dd2b314402d80ab68b891617efdb19a5
Young Woo <youngwoo@Youngs-MacBook-Pro.local>
2016-06-16 11:51:05 -0400

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,6 @@
-console.log("JS file is connected to HTML! Woo!")
+// declared variables and assigned card values
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
